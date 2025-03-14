3afb9c0b6323affcf645814da8de6208a3f4a7bd
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-03-14 19:36:05 +0100

    step 3 done

--- src/main/java/org/lessons/java/spring_hitsmoviessongs/controllers/MainController.java
+++ src/main/java/org/lessons/java/spring_hitsmoviessongs/controllers/MainController.java
@@ -8,6 +8,7 @@ import org.lessons.java.spring_hitsmoviessongs.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller   //add this!
@@ -22,7 +23,7 @@ public class MainController {
     }
     private List<Song> getBestSongs() {
         return Arrays.asList(
-                new Song(1, "Shizen"),
+                new Song(1, "Shenzen"),
                 new Song(2, "Hotel Lux"),
                 new Song(3, "Imagine")
         );
@@ -35,7 +36,7 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/movies")
+    @GetMapping({"/movies" ,"/movies/"})  //without ‘/movies/’, if you navigate to localhost/movies/ you will get errors!
     public String moviesPage(
     Model model){
         List<Movie> movies = getBestMovies();
@@ -44,8 +45,17 @@ public class MainController {
         model.addAttribute("bestmovies", bestmovies);
         return "movies";
     }
+    @GetMapping("/movies/{id}")  //if use a path with {}, you must use @PathVariable!
+    public String moviesPage(@PathVariable int id,  //get the exact param from the path
+    Model model){
+        List<Movie> movies = getBestMovies(); 
+        Movie targetmovie = movies.stream().filter(movie->movie.getId() == id).findFirst().orElse(null);
+        model.addAttribute("targetmovie", targetmovie);
+        return "movies";
+    }
 
-    @GetMapping("/songs")
+    //SONGS
+    @GetMapping({"/songs","/songs/"})
     public String songsPage(
     Model model){
         List<Song> songs = getBestSongs();
@@ -54,5 +64,14 @@ public class MainController {
         model.addAttribute("bestsongs", bestsongs);
         return "songs";
     }
-    
+    @GetMapping("/songs/{id}")
+    public String songsPage(@PathVariable int id,
+    Model model){
+        List<Song> songs = getBestSongs();
+        Song targetsong = songs.stream().filter(song->song.getId()==id).findFirst().orElse(null);
+        model.addAttribute("targetsong",targetsong);
+        return "songs";
+    }
+
+
 }
